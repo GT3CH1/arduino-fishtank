075f041b07aafee0969b887a38f31bab087edfd6
Gavin Pease <gavinpease@gmail.com>
2021-07-14 15:17:16 -0600

    Make updates via OTA now

--- fishtank/fishtank.ino
+++ fishtank/fishtank.ino
@@ -1,20 +1,149 @@
-#include <GoogleArduino.h>
+#include <ArduinoOTA.h> 
+#include <ArduinoHttpClient.h>
+
 #include "secret.h"
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <aREST.h>
 
-const int lightPin = 2;
-const int pumpPin = 3;
-const int resetPin = 4;
-int invertedPins[] = {3};
-GoogleArduino automation(resetPin);
+int status = WL_IDLE_STATUS;
+aREST rest = aREST();
+WiFiServer server(80);
+int LIGHT_PIN = 2;
+int PUMP_PIN = 3;
+bool fishtank_light;
+bool fishtank_pump;
 
-void setup(){
-  automation.setup(FIREBASE_HOST,FIREBASE_AUTH,WIFI_SSID,WIFI_PASSWORD,invertedPins);
-  automation.firstRun("fishtank-light",lightPin);
-  automation.firstRun("fishtank-pump",pumpPin);
+
+void handleSketchDownload() {
+
+  const char* PATH = "/ota/arduino/update-%s-%d.bin";
+  const unsigned long CHECK_INTERVAL = 10000;
+
+  static unsigned long previousMillis;
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis < CHECK_INTERVAL)
+    return;
+  previousMillis = currentMillis;
+
+  WiFiClient transport;
+  HttpClient client(transport, UPDATE_SERVER, SERVER_PORT);
+  Serial.println(String(UPDATE_SERVER) + String(PATH));
+  char buff[50];
+  snprintf(buff, sizeof(buff), PATH, DEVICE_TYPE, VERSION + 1);
+
+  Serial.print("Check for update file ");
+  Serial.println(buff);
+
+  client.get(buff);
+
+  int statusCode = client.responseStatusCode();
+  Serial.print("Update status code: ");
+  Serial.println(statusCode);
+  if (statusCode != 200) {
+    client.stop();
+    return;
+  }
+
+  long length = client.contentLength();
+  if (length == HttpClient::kNoContentLengthHeader) {
+    client.stop();
+    Serial.println("Server didn't provide Content-length header. Can't continue with update.");
+    return;
+  }
+  Serial.print("Server returned update file of size ");
+  Serial.print(length);
+  Serial.println(" bytes");
+
+  if (!InternalStorage.open(length)) {
+    client.stop();
+    Serial.println("There is not enough space to store the update. Can't continue with update.");
+    return;
+  }
+  byte b;
+  while (length > 0) {
+    if (!client.readBytes(&b, 1)) // reading a byte with timeout
+      break;
+    InternalStorage.write(b);
+    length--;
+  }
+  InternalStorage.close();
+  client.stop();
+  if (length > 0) {
+    Serial.print("Timeout downloading update file at ");
+    Serial.print(length);
+    Serial.println(" bytes. Can't continue with update.");
+    return;
+  }
+
+  Serial.println("Sketch update apply and reset.");
+  Serial.flush();
+  InternalStorage.apply(); // this doesn't return
 }
 
-void loop(){
-  automation.checkStatus("fishtank-light",lightPin);
-  automation.checkStatus("fishtank-pump",pumpPin);
-  delay(5000);
+void setup() {
+  // put your setup code here, to run once:
+  server.begin();
+  rest.variable("connected",&status);
+  rest.variable("c91124eb-74fb-41b7-b048-4f139d7587cf",&fishtank_light);
+  rest.variable("08b3f4fe-f8ad-4687-9be7-94d313eb0391",&fishtank_pump);
+  rest.function("on", setOn);
+  rest.function("off", setOff);
+}
+
+void checkAndConnectWifi(){
+  status = WiFi.status();
+  if(status != WL_CONNECTED){
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.print("WiFi disconnected, connecting to wifi.");
+    while(status != WL_CONNECTED){
+      status = WiFi.begin(WIFI_SSID,WIFI_PASSWORD);
+      Serial.print(".");
+      delay(3000);
+    }
+    Serial.println("");
+    Serial.println("Connected");
+    digitalWrite(LED_BUILTIN, LOW);
+    printIP();
+  }
+}
+
+void printIP(){
+    IPAddress ip = WiFi.localIP();
+    Serial.println("IP Address: ");
+    Serial.println(ip);
+}
+  
+void loop() {
+  checkAndConnectWifi();
+  WiFiClient client = server.available();
+  rest.handle(client);
+  handleSketchDownload();
+}
+
+int setOn(String guid){
+    checkGuid(guid,true);
+    return 1;
+}
+
+int setOff(String guid){
+    checkGuid(guid,false);
+    return 1;
+}
+
+void checkGuid(String guid, bool state){
+  if (guid == "c91124eb-74fb-41b7-b048-4f139d7587cf"){
+    pinMode(LIGHT_PIN,OUTPUT);
+    digitalWrite(LIGHT_PIN, state ? LOW : HIGH);
+    fishtank_light = state;
+  }
+  if (guid == "08b3f4fe-f8ad-4687-9be7-94d313eb0391"){
+    pinMode(PUMP_PIN,OUTPUT);
+    digitalWrite(PUMP_PIN, state ? HIGH : LOW);
+    fishtank_pump = state;
+  }
+ 
 }
--- fishtank/secret.h
+++ fishtank/secret.h
@@ -2,12 +2,17 @@
 #define secret_h
 
 // The URL to your firebase host - no http:// or https://
-static String FIREBASE_HOST = "";
+static String FIREBASE_HOST = "new-project-3cddb-default-rtdb.firebaseio.com";
 // The authentication key
-static String FIREBASE_AUTH = "";
+static String FIREBASE_AUTH = "7ZnZXL7mXVoqv8h4nxalIvMjMMc7zj97is4AxJyL";
 // Your wifi network
-static char WIFI_SSID[] = "";
+static char WIFI_SSID[] = "peasenet-iot";
 // Your wifi network password.
-static char WIFI_PASSWORD[] = "";
+static char WIFI_PASSWORD[] = "peasenetiot2019";
+
+const short VERSION = 0;
+const char* DEVICE_TYPE = "fishtank";
+const char* UPDATE_SERVER = "api.peasenet.com";
+const unsigned short SERVER_PORT = 80;
 
 #endif
