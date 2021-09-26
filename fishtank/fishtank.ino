07cc35c986294c44409096851800bfd7755823be
Gavin Pease <gavinpease@gmail.com>
2021-09-26 10:26:11 -0600

    Make getting data from server faster

--- fishtank/fishtank.ino
+++ fishtank/fishtank.ino
@@ -1,6 +1,6 @@
-#include <ArduinoOTA.h> 
+#include <ArduinoOTA.h>
 #include <ArduinoHttpClient.h>
-
+#include <ArduinoJson.h>
 #include "secret.h"
 #include <SPI.h>
 #include <WiFiNINA.h>
@@ -13,117 +13,114 @@ int LIGHT_PIN = 2;
 int PUMP_PIN = 3;
 bool fishtank_light;
 bool fishtank_pump;
-
+StaticJsonDocument<512> doc;
 
 void handleSketchDownload() {
+    const char* PATH = "/ota/arduino/update-%s-%d.bin";
+    const unsigned long CHECK_INTERVAL = 10000;
 
-  const char* PATH = "/ota/arduino/update-%s-%d.bin";
-  const unsigned long CHECK_INTERVAL = 10000;
+    static unsigned long previousMillis;
 
-  static unsigned long previousMillis;
+    unsigned long currentMillis = millis();
 
-  unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis < CHECK_INTERVAL)
+        return;
+    previousMillis = currentMillis;
 
-  if (currentMillis - previousMillis < CHECK_INTERVAL)
-    return;
-  previousMillis = currentMillis;
+    WiFiClient transport;
+    HttpClient client(transport, UPDATE_SERVER, SERVER_PORT);
+    Serial.println(String(UPDATE_SERVER) + String(PATH));
+    char buff[50];
+    snprintf(buff, sizeof(buff), PATH, DEVICE_TYPE, VERSION + 1);
 
-  WiFiClient transport;
-  HttpClient client(transport, UPDATE_SERVER, SERVER_PORT);
-  Serial.println(String(UPDATE_SERVER) + String(PATH));
-  char buff[50];
-  snprintf(buff, sizeof(buff), PATH, DEVICE_TYPE, VERSION + 1);
+    Serial.print("Check for update file ");
+    Serial.println(buff);
 
-  Serial.print("Check for update file ");
-  Serial.println(buff);
+    client.get(buff);
 
-  client.get(buff);
+    int statusCode = client.responseStatusCode();
+    Serial.print("Update status code: ");
+    Serial.println(statusCode);
+    if (statusCode != 200) {
+        client.stop();
+        return;
+    }
 
-  int statusCode = client.responseStatusCode();
-  Serial.print("Update status code: ");
-  Serial.println(statusCode);
-  if (statusCode != 200) {
-    client.stop();
-    return;
-  }
-
-  long length = client.contentLength();
-  if (length == HttpClient::kNoContentLengthHeader) {
-    client.stop();
-    Serial.println("Server didn't provide Content-length header. Can't continue with update.");
-    return;
-  }
-  Serial.print("Server returned update file of size ");
-  Serial.print(length);
-  Serial.println(" bytes");
-
-  if (!InternalStorage.open(length)) {
-    client.stop();
-    Serial.println("There is not enough space to store the update. Can't continue with update.");
-    return;
-  }
-  byte b;
-  while (length > 0) {
-    if (!client.readBytes(&b, 1)) // reading a byte with timeout
-      break;
-    InternalStorage.write(b);
-    length--;
-  }
-  InternalStorage.close();
-  client.stop();
-  if (length > 0) {
-    Serial.print("Timeout downloading update file at ");
+    long length = client.contentLength();
+    if (length == HttpClient::kNoContentLengthHeader) {
+        client.stop();
+        Serial.println("Server didn't provide Content-length header. Can't continue with update.");
+        return;
+    }
+    Serial.print("Server returned update file of size ");
     Serial.print(length);
-    Serial.println(" bytes. Can't continue with update.");
-    return;
-  }
+    Serial.println(" bytes");
 
-  Serial.println("Sketch update apply and reset.");
-  Serial.flush();
-  InternalStorage.apply(); // this doesn't return
+    if (!InternalStorage.open(length)) {
+        client.stop();
+        Serial.println("There is not enough space to store the update. Can't continue with update.");
+        return;
+    }
+    byte b;
+    while (length > 0) {
+        if (!client.readBytes(&b, 1)) // reading a byte with timeout
+            break;
+        InternalStorage.write(b);
+        length--;
+    }
+    InternalStorage.close();
+    client.stop();
+    if (length > 0) {
+        Serial.print("Timeout downloading update file at ");
+        Serial.print(length);
+        Serial.println(" bytes. Can't continue with update.");
+        return;
+    }
+
+    Serial.println("Sketch update apply and reset.");
+    Serial.flush();
+    InternalStorage.apply(); // this doesn't return
 }
 
 void setup() {
-  // put your setup code here, to run once:
-  server.begin();
-  rest.variable("connected",&status);
-  rest.function("on", setOn);
-  rest.function("off", setOff);
+    server.begin();
+    rest.variable("connected",&status);
+    rest.function("on", setOn);
+    rest.function("off", setOff);
 }
 
 void checkAndConnectWifi(){
-  status = WiFi.status();
-  if(status != WL_CONNECTED){
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, HIGH);
-    Serial.print("WiFi disconnected, connecting to wifi.");
-    while(status != WL_CONNECTED){
-      status = WiFi.begin(WIFI_SSID,WIFI_PASSWORD);
-      Serial.print(".");
-      delay(3000);
+    status = WiFi.status();
+    if(status != WL_CONNECTED){
+        pinMode(LED_BUILTIN, OUTPUT);
+        digitalWrite(LED_BUILTIN, HIGH);
+        Serial.print("WiFi disconnected, connecting to wifi.");
+        while(status != WL_CONNECTED){
+            status = WiFi.begin(WIFI_SSID,WIFI_PASSWORD);
+            Serial.print(".");
+            delay(100);
+        }
+        Serial.println("");
+        Serial.println("Connected");
+        digitalWrite(LED_BUILTIN, LOW);
+        printIP();
+        bool last_state = getLastState(LIGHT_GUID);
+        checkGuid(LIGHT_GUID, last_state);
+        last_state = getLastState(PUMP_GUID);
+        checkGuid(PUMP_GUID, last_state);
     }
-    Serial.println("");
-    Serial.println("Connected");
-    digitalWrite(LED_BUILTIN, LOW);
-    printIP();
-    bool last_state = getLastState(LIGHT_GUID);
-    checkGuid(LIGHT_GUID, last_state);
-    last_state = getLastState(PUMP_GUID);
-    checkGuid(PUMP_GUID, last_state);
-  }
 }
 
 void printIP(){
-    IPAddress ip = WiFi.localIP();
     Serial.println("IP Address: ");
-    Serial.println(ip);
+    Serial.println(getIP());
 }
-  
+
 void loop() {
-  checkAndConnectWifi();
-  WiFiClient client = server.available();
-  rest.handle(client);
-  handleSketchDownload();
+    checkAndConnectWifi();
+    WiFiClient client = server.available();
+    rest.handle(client);
+    handleSketchDownload();
 }
 
 int setOn(String guid){
@@ -137,49 +134,61 @@ int setOff(String guid){
 }
 
 void checkGuid(String guid, bool state){
-  if (guid == LIGHT_GUID){
-    pinMode(LIGHT_PIN,OUTPUT);
-    digitalWrite(LIGHT_PIN, state ? LOW : HIGH);
-    fishtank_light = state;
-  }
-  if (guid == PUMP_GUID){
-    pinMode(PUMP_PIN,OUTPUT);
-    digitalWrite(PUMP_PIN, state ? HIGH : LOW);
-    fishtank_pump = state;
-  }
-  sendUpdate(guid,state);
- 
+    if (guid == LIGHT_GUID){
+        pinMode(LIGHT_PIN,OUTPUT);
+        digitalWrite(LIGHT_PIN, state ? LOW : HIGH);
+        fishtank_light = state;
+    }
+    if (guid == PUMP_GUID){
+        pinMode(PUMP_PIN,OUTPUT);
+        digitalWrite(PUMP_PIN, state ? HIGH : LOW);
+        fishtank_pump = !state;
+        state = state;
+    }
+    sendUpdate(guid,state);
+
 }
 
 void sendUpdate(String guid, bool state){
-  WiFiClient wifi;
-  HttpClient httpClient = HttpClient(wifi, UPDATE_SERVER, SERVER_PORT);
-
-  String contentType = "application/x-www-form-urlencoded";
-  String data = "guid=" + guid + "&ip=" + IpAddress2String(WiFi.localIP()) + "&state=" + state + "&sw_version=" + String(VERSION);
-  Serial.println(data);
-  httpClient.put("/smarthome/update",contentType,data);
-  int statusCode = httpClient.responseStatusCode();
-
-  Serial.print("Update status code: ");
-  Serial.println(statusCode);
-  httpClient.flush();
-  httpClient.stop();
+    WiFiClient wifi;
+    HttpClient httpClient = HttpClient(wifi, UPDATE_SERVER, SERVER_PORT);
+    String contentType = "application/x-www-form-urlencoded";
+    String data = "guid=" + guid + "&ip=" + getIP() + "&state=" + (state ? "true" : "false") + "&sw_version=" + String(VERSION);
+    httpClient.sendHeader("x-api-key", API_KEY);
+    httpClient.sendHeader("x-auth-id", API_ID);
+    httpClient.sendHeader(HTTP_HEADER_CONTENT_LENGTH, data.length());
+    Serial.println(data);
+    httpClient.put("/smarthome/update",contentType,data);
+    int statusCode = httpClient.responseStatusCode(); 
+    httpClient.stop();
 }
 
 bool getLastState(String guid){
-  WiFiClient wifi;
-  HttpClient httpClient = HttpClient(wifi, UPDATE_SERVER, SERVER_PORT);
-  httpClient.get("/smarthome/device/"+guid);
-  String response = httpClient.responseBody();
-  return response == "true";
+    WiFiClient wifi;
+    Serial.println("Getting last state for " + guid);
+    HttpClient httpClient = HttpClient(wifi, UPDATE_SERVER, SERVER_PORT);
+    httpClient.beginRequest();
+    httpClient.get("/smarthome/device/"+guid);
+    httpClient.sendHeader("x-api-key", API_KEY);
+    httpClient.sendHeader("x-auth-id", API_ID);
+    httpClient.endRequest();
+    String responeBody = httpClient.responseBody();
+    DeserializationError error = deserializeJson(doc, responeBody.c_str(), 512);
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return false;
+    }
+    Serial.println(responeBody);
+    return doc["last_state"];
 }
 
 
-String IpAddress2String(const IPAddress& ipAddress)
+String getIP()
 {
-  return String(ipAddress[0]) + String(".") +\
-  String(ipAddress[1]) + String(".") +\
-  String(ipAddress[2]) + String(".") +\
-  String(ipAddress[3])  ; 
+  const IPAddress& ipAddress = WiFi.localIP();
+    return String(ipAddress[0]) + String(".") +\
+        String(ipAddress[1]) + String(".") +\
+        String(ipAddress[2]) + String(".") +\
+        String(ipAddress[3])  ;
 }
--- fishtank/secret.h
+++ fishtank/secret.h
@@ -10,12 +10,14 @@ static char WIFI_SSID[] = "peasenet-iot";
 // Your wifi network password.
 static char WIFI_PASSWORD[] = "peasenetiot2019";
 
-const short VERSION = 3;
+const short VERSION = 6;
 const char* DEVICE_TYPE = "fishtank";
 const char* UPDATE_SERVER = "api.peasenet.com";
 const unsigned short SERVER_PORT = 80;
 
 const String LIGHT_GUID = "c91124eb-74fb-41b7-b048-4f139d7587cf";
 const String PUMP_GUID = "08b3f4fe-f8ad-4687-9be7-94d313eb0391";
+const String API_KEY ="e196c2ec-7cef-467a-c6e5-f6f20d2a7103";
+const String API_ID = "icQ8XYD6gKNywSWnysGhNj68xHe2";
 
 #endif
